aff0d85a70f7e816eef55ff83928572fa4fdd5a2
Stanislau Yarmats <122651968+yarmatss@users.noreply.github.com>
2025-05-22 11:00:57 +0200

    Create zad3.ino

---
+++ zad3.ino
@@ -0,0 +1,235 @@
+#include "DHT.h"
+
+DHT dht;
+
+int licznik = 0;
+int liczba = 0;
+int minimum, maksimum, jasnosc, wilgotnosc, temperatura; 
+char tryb = 'L'; // L - jasność (Light), H - wilgotność (Humidity), T - temperatura
+
+// Funkcja wyświetlająca znak na wyświetlaczu 7-segmentowym
+void Znak(int iNumer) {
+    // Dla wyświetlacza common anode, LOW włącza segment, HIGH wyłącza
+    // Pinowanie: D2=a, D3=b, D4=c, D5=d, D6=e, D7=f, D8=g
+    switch (iNumer) {
+        case 0: // 0: włączamy a,b,c,d,e,f wyłączamy g
+            digitalWrite(2, LOW);  // a
+            digitalWrite(3, LOW);  // b
+            digitalWrite(4, LOW);  // c
+            digitalWrite(5, LOW);  // d
+            digitalWrite(6, LOW);  // e
+            digitalWrite(7, LOW);  // f
+            digitalWrite(8, HIGH); // g
+            break;
+        case 1: // 1: włączamy b,c wyłączamy a,d,e,f,g
+            digitalWrite(2, HIGH); // a
+            digitalWrite(3, LOW);  // b
+            digitalWrite(4, LOW);  // c
+            digitalWrite(5, HIGH); // d
+            digitalWrite(6, HIGH); // e
+            digitalWrite(7, HIGH); // f
+            digitalWrite(8, HIGH); // g
+            break;
+        case 2: // 2: włączamy a,b,g,e,d wyłączamy c,f
+            digitalWrite(2, LOW);  // a
+            digitalWrite(3, LOW);  // b
+            digitalWrite(4, HIGH); // c
+            digitalWrite(5, LOW);  // d
+            digitalWrite(6, LOW);  // e
+            digitalWrite(7, HIGH); // f
+            digitalWrite(8, LOW);  // g
+            break;
+        case 3: // 3: włączamy a,b,c,d,g wyłączamy e,f
+            digitalWrite(2, LOW);  // a
+            digitalWrite(3, LOW);  // b
+            digitalWrite(4, LOW);  // c
+            digitalWrite(5, LOW);  // d
+            digitalWrite(6, HIGH); // e
+            digitalWrite(7, HIGH); // f
+            digitalWrite(8, LOW);  // g
+            break;
+        case 4: // 4: włączamy b,c,f,g wyłączamy a,d,e
+            digitalWrite(2, HIGH); // a
+            digitalWrite(3, LOW);  // b
+            digitalWrite(4, LOW);  // c
+            digitalWrite(5, HIGH); // d
+            digitalWrite(6, HIGH); // e
+            digitalWrite(7, LOW);  // f
+            digitalWrite(8, LOW);  // g
+            break;
+        case 5: // 5: włączamy a,c,d,f,g wyłączamy b,e
+            digitalWrite(2, LOW);  // a
+            digitalWrite(3, HIGH); // b
+            digitalWrite(4, LOW);  // c
+            digitalWrite(5, LOW);  // d
+            digitalWrite(6, HIGH); // e
+            digitalWrite(7, LOW);  // f
+            digitalWrite(8, LOW);  // g
+            break;
+        case 6: // 6: włączamy a,c,d,e,f,g wyłączamy b
+            digitalWrite(2, LOW);  // a
+            digitalWrite(3, HIGH); // b
+            digitalWrite(4, LOW);  // c
+            digitalWrite(5, LOW);  // d
+            digitalWrite(6, LOW);  // e
+            digitalWrite(7, LOW);  // f
+            digitalWrite(8, LOW);  // g
+            break;
+        case 7: // 7: włączamy a,b,c wyłączamy d,e,f,g
+            digitalWrite(2, LOW);  // a
+            digitalWrite(3, LOW);  // b
+            digitalWrite(4, LOW);  // c
+            digitalWrite(5, HIGH); // d
+            digitalWrite(6, HIGH); // e
+            digitalWrite(7, HIGH); // f
+            digitalWrite(8, HIGH); // g
+            break;
+        case 8: // 8: włączamy wszystkie segmenty
+            digitalWrite(2, LOW);  // a
+            digitalWrite(3, LOW);  // b
+            digitalWrite(4, LOW);  // c
+            digitalWrite(5, LOW);  // d
+            digitalWrite(6, LOW);  // e
+            digitalWrite(7, LOW);  // f
+            digitalWrite(8, LOW);  // g
+            break;
+        case 9: // 9: włączamy a,b,c,d,f,g wyłączamy e
+            digitalWrite(2, LOW);  // a
+            digitalWrite(3, LOW);  // b
+            digitalWrite(4, LOW);  // c
+            digitalWrite(5, LOW);  // d
+            digitalWrite(6, HIGH); // e
+            digitalWrite(7, LOW);  // f
+            digitalWrite(8, LOW);  // g
+            break;
+        default: // Nieznany symbol - wyświetl "0"
+            digitalWrite(2, LOW);  // a
+            digitalWrite(3, LOW);  // b
+            digitalWrite(4, LOW);  // c
+            digitalWrite(5, LOW);  // d
+            digitalWrite(6, HIGH); // e
+            digitalWrite(7, LOW);  // f
+            digitalWrite(8, LOW);  // g
+            break;
+    }
+}
+
+// Poprawiona funkcja do wyświetlania literki trybu na lewym wyświetlaczu
+void WyswietlTryb(char t) {
+    switch(t) {
+        case 'L': // Tryb jasności - litera L
+            digitalWrite(2, HIGH); // a - wyłączony
+            digitalWrite(3, LOW); // b - wyłączony
+            digitalWrite(4, LOW); // c - wyłączony
+            digitalWrite(5, HIGH);  // d - włączony
+            digitalWrite(6, LOW);  // e - włączony
+            digitalWrite(7, LOW);  // f - włączony
+            digitalWrite(8, LOW); // g - wyłączony
+            break;
+        case 'H': // Tryb wilgotności - litera H
+            digitalWrite(2, HIGH); // a - wyłączony
+            digitalWrite(3, HIGH);  // b - włączony
+            digitalWrite(4, HIGH);  // c - włączony
+            digitalWrite(5, LOW); // d - wyłączony
+            digitalWrite(6, LOW);  // e - włączony
+            digitalWrite(7, LOW);  // f - włączony
+            digitalWrite(8, LOW);  // g - włączony
+            break;
+        case 'T': // Tryb temperatury - litera t (mała)          
+            digitalWrite(2, HIGH); // a - wyłączony
+            digitalWrite(3, HIGH); // b - wyłączony
+            digitalWrite(4, HIGH); // c - wyłączony
+            digitalWrite(5, LOW);  // d - włączony
+            digitalWrite(6, LOW);  // e - włączony
+            digitalWrite(7, LOW);  // f - włączony
+            digitalWrite(8, HIGH);  // g - włączony
+            break;
+    }
+}
+
+void setup() { 
+    // Inicjalizacja czujnika DHT11 na pinie 12
+    dht.setup(12); 
+    
+    // Konfiguracja pinów do wyświetlaczy jako wyjścia
+    for (int i = 2; i <= 8; i++) 
+        pinMode(i, OUTPUT);
+    
+    pinMode(10, OUTPUT); // Kontrola wyświetlacza dziesiątek
+    pinMode(11, OUTPUT); // Kontrola wyświetlacza jedności
+    
+    // Konfiguracja przycisku z INPUT_PULLUP na pinie D9
+    pinMode(9, INPUT_PULLUP);
+    
+    // Początkowa kalibracja czujnika światła
+    delay(1000); 
+    minimum = analogRead(0);
+    maksimum = minimum;
+}
+
+void loop() {
+    // Obsługa pierwszej cyfry (jedności)
+    digitalWrite(10, LOW);  // Wyłącz drugi wyświetlacz
+    digitalWrite(11, HIGH); // Włącz pierwszy wyświetlacz
+    Znak(liczba % 10);
+    delay(2);
+    
+    // Obsługa drugiej cyfry (dziesiątki)
+    digitalWrite(11, LOW);  // Wyłącz pierwszy wyświetlacz
+    digitalWrite(10, HIGH); // Włącz drugi wyświetlacz
+    
+    // Jeśli nie wciśnięto przycisku, wyświetl drugą cyfrę
+    if (digitalRead(9) == HIGH) {
+        Znak((liczba / 10) % 10);
+    } else {
+        // Jeśli wciśnięto przycisk, wyświetl tryb
+        WyswietlTryb(tryb);
+        
+        // Zmień tryb po zwolnieniu przycisku
+        delay(200); // Debounce
+        
+        while (digitalRead(9) == LOW) {
+            // Czekaj na zwolnienie przycisku
+        }
+        
+        // Przełącz na następny tryb
+        if (tryb == 'L')
+            tryb = 'H';
+        else if (tryb == 'H')
+            tryb = 'T';
+        else
+            tryb = 'L';
+            
+        delay(200); // Debounce
+    }
+    delay(2);
+    
+    // Pomiar i aktualizacja wartości co około 200ms
+    if (licznik++ > 100) {
+        // Pomiar jasności
+        jasnosc = analogRead(0);
+        if (minimum > jasnosc) minimum = jasnosc;
+        if (maksimum < jasnosc) maksimum = jasnosc;
+        if (minimum != maksimum) 
+            jasnosc = 99 * (jasnosc - minimum) / (maksimum - minimum);
+        
+        // Pomiar wilgotności i temperatury z DHT11
+        wilgotnosc = (int)(dht.getHumidity());
+        temperatura = (int)(dht.getTemperature());
+        
+        // Ustaw odpowiednią wartość do wyświetlenia zgodnie z trybem
+        switch(tryb) {
+            case 'L': 
+                liczba = jasnosc; 
+                break;
+            case 'H': 
+                liczba = wilgotnosc; 
+                break;
+            case 'T': 
+                liczba = temperatura; 
+                break;
+        }
+        
+        licznik = 0;
+    }
+}
